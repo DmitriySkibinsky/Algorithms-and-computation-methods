065799788577ed7b175f6fdea2d693100465590b
Dmitriy <70383822+DmitriySkibinsky@users.noreply.github.com>
2024-07-11 01:13:52 +0300

    Update 1. Simson.py

--- source/1. Numerical Integration/1. Simson.py
+++ source/1. Numerical Integration/1. Simson.py
@@ -8,21 +8,28 @@ def f(x):
 a = 0
 b = 3
 
-# Probability density function (PDF) for generating random points
-# In this case, we will use a uniform PDF on the interval [a, b]
-def pdf(x):
-    return np.where((a <= x) & (x <= b), 1 / (b - a), 0)
+# Number of subintervals (should be an even number)
+n = 1000
 
-# Number of random points for the Monte Carlo method
-num_samples = 100000
+# Simpson's method
+def simpson(f, a, b, n):
+    h = (b - a) / n
+    x = np.linspace(a, b, n+1)
+    y = f(x)
+    
+    integral = y[0] + y[-1]
+    
+    for i in range(1, n, 2):
+        integral += 4 * y[i]
+    
+    for i in range(2, n-1, 2):
+        integral += 2 * y[i]
+    
+    integral *= h / 3
+    return integral
 
-# Generate random points and compute the integral
-random_x = np.random.uniform(a, b, num_samples)
-f_values = f(random_x)
-pdf_values = pdf(random_x)
-
-# Compute the integral estimate
-integral_estimate = np.mean(f_values / pdf_values)
+# Calculate the integral
+integral_value = simpson(f, a, b, n)
 
 # Print the result
-print("Approximate value of the integral:", integral_estimate)
+print("Approximate value of the integral using Simpson's method:", integral_value)
